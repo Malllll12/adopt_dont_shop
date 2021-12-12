e65ce25c988abd56a8cab2cf9cfb68c96f8f094a
Mallory Vining <mallorygallery12@gmail.com>
2021-12-12 13:02:01 -0700

    Add User story 2

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,7 +7,18 @@ class ApplicationsController < ApplicationController
 
   end
 
+  def create
+    application = Application.create!(app_params)
+
+    redirect_to "/applications/#{application.id}"
+  end
+
   def show
     @application = Application.find(params[:id])
+  end
+
+  private
+  def app_params
+    params.permit(:id, :applicant_name, :address, :city, :state, :zip_code, :reason, :status)
   end
 end
--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -1,19 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Application index page', type: :feature do
-# As a visitor
-# When I visit the pet index page
-# Then I see a link to "Start an Application"
-# When I click this link
-# Then I am taken to the new application page where I see a form
-# When I fill in this form with my:
-#   - Name
-#   - Street Address
-#   - City
-#   - State
-#   - Zip Code
-# And I click submit
-# Then I am taken to the new application's show page
 # And I see my Name, address information, and description of why I would make a good home
 # And I see an indicator that this application is "In Progress"
   it 'links to a new application form' do
